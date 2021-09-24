10b2769175e8bc91743e9712f2df89388f888bcf
junho5 <dhwnsghdhkd@naver.com>
2021-09-25 00:48:16 +0900

    float_split2

--- codeup/test.py
+++ codeup/test.py
@@ -1,2 +1,8 @@
-a,b=input().split(' ')
-print(format(round(float(a)/float(b),4), ".4f") )
+a,b,c=input().split(' ')
+a= int(a)
+b= int(b)
+c= int(c)
+mean = ((a+b+c)/3)
+d = format(mean,".2f")
+print("{} {}".format(a+b+c,d))
+
