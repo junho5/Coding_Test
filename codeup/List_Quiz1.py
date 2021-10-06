ca0a35c022ee9e146cc607f4462c3f3235b2e984
junho5 <dhwnsghdhkd@naver.com>
2021-10-06 23:18:26 +0900

    study

--- codeup/List_Quiz1.py
+++ codeup/List_Quiz1.py
@@ -7,3 +7,15 @@ for i in range(5):
     for j in range(5):
         a[j]=int(a[j])
         answer[i].append(a[j])
+# for i in range(5):
+#     for j in range(5):
+#         print(answer[i][j], end=' ')    
+#     print() 
+
+n=int(input())
+for i in range(n):
+    a,b=input().split(' ')
+    a=int(a)
+    b=int(b)
+    for j in range(n):
+        if a==answe
