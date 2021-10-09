21bb9b506353caec84167738bf432ebbb12dbc5b
junho5 <dhwnsghdhkd@naver.com>
2021-10-09 22:30:07 +0900

    sugar draw clear

--- codeup/sugar_draw.py
+++ codeup/sugar_draw.py
@@ -1,20 +1,20 @@
-h,w=map(int,input().split(' '))
-pan=[]
+h, w = map(int, input().split(' '))
+pan = []
 
 for i in range(h):
     pan.append([])
     for j in range(w):
         pan[i].append(0)
-n=int(input())
+n = int(input())
 for i in range(n):
-    l,d,x,y=map(int,input().split(' '))
+    l, d, x, y = map(int, input().split(' '))
     for j in range(l):
-        if d==0:
-            pan[x-1][y-1+j]=1
+        if d == 0:
+            pan[x-1][y-1+j] = 1
         else:
-            pan[x-1+j][y-1]=1
+            pan[x-1+j][y-1] = 1
 
 for i in range(h):
     for j in range(w):
-        print(pan[i][j],end=' ')
-    print()            
+        print(pan[i][j], end=' ')
+    print()
