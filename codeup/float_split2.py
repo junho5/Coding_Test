5f2b8609e59d7e5c6fd7ec0e0b0e7d7bdc46dbc7
junho5 <dhwnsghdhkd@naver.com>
2021-10-10 22:30:20 +0900

    float_split

--- codeup/float_split2.py
+++ codeup/float_split2.py
@@ -1,10 +1,9 @@
 # 필요한 저장 공간을 MB 단위로 바꾸어 출력한다.
-# 단, 소수점 셋째 자리에서 반올림하여 둘째 자리까지 출력한다. 
-a,b,c=input().split(' ')
-a=int(a)
-b=int(b)
-c=int(c)
+# 단, 소수점 셋째 자리에서 반올림하여 둘째 자리까지 출력한다.
+a, b, c = input().split(' ')
+a = int(a)
+b = int(b)
+c = int(c)
 total = (((a*b*c)/8)/1024)/1024
-total = round(total,2)
+total = round(total, 2)
 print('{:.2f} MB'.format(total))
-
