f78ce88ce9ea6463737f52f8f32ddf7ee7fc8ac9
junho5 <dhwnsghdhkd@naver.com>
2021-10-29 22:33:09 +0900

    ant finish

--- codeup/test.py
+++ codeup/test.py
@@ -1,7 +1,6 @@
-a, b, c = input().split(' ')
-a = int(a)
-b = int(b)
-c = int(c)
-mean = ((a+b+c)/3)
-d = format(mean, ".2f")
-print("{} {}".format(a+b+c, d))
+X=1
+Y=1
+while (X!=5 and Y!=5):
+    print('no')
+    X+=1
+    Y+=1
