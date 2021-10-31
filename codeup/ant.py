bc30ead0a214497299bbd9086c08a1e324f801c5
junho5 <dhwnsghdhkd@naver.com>
2021-10-31 21:50:27 +0900

    test period

--- codeup/ant.py
+++ codeup/ant.py
@@ -8,25 +8,25 @@ for i in range(10):
 x = 1
 y = 1
 while (True):
-    ant[1][1]=9
-    if ant[x][y+1]==0:
-        ant[x][y+1]=9
-        y+=1
-    elif ant[x][y+1]!=0 and ant[x+1][y]==0:
-        ant[x+1][y]=9
-        x+=1
-    elif ant[x+1][y]==2:
-        ant[x+1][y]=9
+    ant[1][1] = 9
+    if ant[x][y+1] == 0:
+        ant[x][y+1] = 9
+        y += 1
+    elif ant[x][y+1] != 0 and ant[x+1][y] == 0:
+        ant[x+1][y] = 9
+        x += 1
+    elif ant[x+1][y] == 2:
+        ant[x+1][y] = 9
         break
-    elif ant[x][y+1]==2: 
-        ant[x][y+1]=9
+    elif ant[x][y+1] == 2:
+        ant[x][y+1] = 9
         break
-    elif ant[x][y+1]!=0 and ant[x+1][y]!=0:
+    elif ant[x][y+1] != 0 and ant[x+1][y] != 0:
         break
-    elif x==9 and y==9:
+    elif x == 9 and y == 9:
         break
 
 for i in range(10):
     for j in range(10):
-        print(ant[i][j] ,end=' ')
+        print(ant[i][j], end=' ')
     print()
