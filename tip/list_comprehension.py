f2a3c2194b82c34be62cda38f2652067be7a7d29
junho5 <dhwnsghdhkd@naver.com>
2021-11-01 23:54:36 +0900

    test finish

--- tip/list_comprehension.py
+++ tip/list_comprehension.py
@@ -1,10 +1,10 @@
 # 조건문을 넣어서 리스트 초기화 하는 방법
-array = [i for i in range(20) if i%2==0]
+array = [i for i in range(20) if i % 2 == 0]
 print(array)
 
 # 2차원 리스트 0으로 초기화 하는방법
 # 여기서 언더바는 숫자 안쓸때 사용
-n=3
-m=4
+n = 3
+m = 4
 array2 = [[0]*n for _ in range(m)]
 print(array2)
