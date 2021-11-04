5e961ef8046072a13abfa85e8d5431e0f1d421ba
junho5 <dhwnsghdhkd@naver.com>
2021-11-05 01:42:17 +0900

    itertools

---
+++ tip/input_tip.py
@@ -0,0 +1,9 @@
+import sys
+
+data = sys.stdin.readline().rstrip()
+
+print(data)
+
+# input같은 경우 시간을 많이 잡아먹기 때문에 sys를통해 시간을 절약한다.
+
+print(f"{data}")
---
+++ tip/itertools.py
@@ -0,0 +1,27 @@
+# 파이썬에서 반복되는 데이터를 처리하는 기능을 포함하고 있는 라이브러리
+
+from itertools import permutations
+# 만들수 있는 모든 순열 출력 (숫자로 개수 지정가능)
+data = ['A', 'B', 'C']
+data = list(permutations(data, 2))
+print(data)
+
+from itertools import product
+# 만들수있는 모은 순열 출력 (숫자로 개수 지정가능, 중복 허용)
+data = ['A', 'B', 'C']
+data = list(product(data, repeat=2))
+print(data)
+
+print('------------------------------------------------------------------------------------------------------------------------------')
+
+from itertools import combinations
+# 만들수있는 조합 출력 (숫자로 개수 지정가능)
+data = ['A', 'B', 'C']
+data = list(combinations(data, 2))
+print(data)
+
+from itertools import combinations_with_replacement
+# 만들수있는 조합 출력 (숫자로 개수 지정가능, 중복 허용)
+data = ['A', 'B', 'C']
+data = list(combinations_with_replacement(data, 2))
+print(data)
