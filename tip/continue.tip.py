faa10eb6732dbfa3ba2a5174f12ce011f3336b06
junho5 <dhwnsghdhkd@naver.com>
2021-11-05 22:54:45 +0900

    tip finish

--- tip/continue.tip.py
+++ tip/continue.tip.py
@@ -1,6 +1,6 @@
-for i in range(2,10):
-    for j in range(1,10):
-        if i%2 ==0:
+for i in range(2, 10):
+    for j in range(1, 10):
+        if i % 2 == 0:
             continue
-        print('{} * {} = {}'.format(i,j,i*j))
+        print('{} * {} = {}'.format(i, j, i*j))
     print(' ')
--- tip/dict.tip.py
+++ tip/dict.tip.py
@@ -1,11 +1,11 @@
-a={'사과': 'apple'} # 이 방법은 처음에 나중에 하면 초기화 되버림 
-a['바나나']='banana'
-a[2]='two'
+a = {'사과': 'apple'}  # 이 방법은 처음에 나중에 하면 초기화 되버림
+a['바나나'] = 'banana'
+a[2] = 'two'
 
-b=dict()
-b={'오렌지':'orange'} # 이 방법은 처음에 나중에 하면 초기화 되버림
-b[1]='one'
-b['melon']='멜론'
+b = dict()
+b = {'오렌지': 'orange'}  # 이 방법은 처음에 나중에 하면 초기화 되버림
+b[1] = 'one'
+b['melon'] = '멜론'
 
 print(a['사과'])
 print(a[2])
@@ -24,11 +24,9 @@ print(a.keys())
 print(a.items())
 print('----------------------')
 
-a.clear() # 다 지우기
+a.clear()  # 다 지우기
 print(b['melon'])
 print(b.get('melon'))
 print('----------------------')
 print('melon' in b)
 print('----------------------')
-
-
