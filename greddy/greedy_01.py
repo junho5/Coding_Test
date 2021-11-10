3c0a16e4dde4a196bd4c30e0cacc0f6a032b5afd
junho5 <dhwnsghdhkd@naver.com>
2021-11-10 22:05:12 +0900

    greedy 01 finish

--- greddy/greedy_01.py
+++ greddy/greedy_01.py
@@ -1,13 +1,12 @@
-def calculate_change(payment,cost):
+def calculate_change(payment, cost):
     change = payment-cost
-    coin_type=[50000,10000,5000,1000]
+    coin_type = [50000, 10000, 5000, 1000]
     money = 0
     for coin in coin_type:
         money = change // coin
         print(f"{change}원 잔돈으로 {coin}원{money}장 거슬러줌")
         change %= coin
         print(f"{change}원 잔돈 남았음")
-        
-    
-    
-calculate_change(100000,33000)
+
+
+calculate_change(100000, 33000)
