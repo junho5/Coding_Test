5f1f9e64813a035e7849f029a5871d674944cfb7
junho5 <dhwnsghdhkd@naver.com>
2021-11-17 23:09:34 +0900

    greedy 03

---
+++ greddy/greedy_03.py
@@ -0,0 +1,11 @@
+# 행에서 가장 작은 숫자를 뽑을때 가장 큰 값을 가지고 있는 행의 작은값 가져오기
+N, M = map(int,input().split(' '))
+
+result = 0
+
+for i in range(N):
+    data = list(map(int,input().split(' ')))
+    min_value = min(data)
+    result = max(result,min_value)
+
+print(result)    
