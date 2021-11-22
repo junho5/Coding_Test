c3071ec903f4c4bf260688b84abe811024f76d64
junho5 <dhwnsghdhkd@naver.com>
2021-11-22 23:18:23 +0900

    friend homework

--- greddy/test.py
+++ greddy/test.py
@@ -4,42 +4,48 @@ for i in range(n):
     data = list(map(int, input().split(' ')))
     arr.append(data)
 
-arr.sort(key=lambda x: (-x[0], -x[1]))
-print(arr)
+arr.sort(key=lambda x: (-x[0], -x[1], -x[2]))
+# print(arr)
 
-index = 0
+index = 0  
 first = 1
 second = 0
 third = 0
 
 for i in range(n-1):
+    if sum(arr[i]) != sum(arr[i+1]):
+        break    
     if sum(arr[i]) == sum(arr[i+1]):
-       index += 1
-       first += 1
+        first += 1
+        index += 1
 
-print(index, first, second, third)
+
+# print(f'index = {index} first = {first} second = {second} third = {third}')
 for i in range(index, n-1):
-    if index >= n-1:
+    if sum(arr[i]) == sum(arr[i+1]):
+        index += 1
+        second += 2
+    if index > n-1:
         break
     else:
         index += 1
         second += 1
-    if sum(arr[i]) == sum(arr[i+1]):
-       index += 1
-       second += 1
-print(index, first, second, third)
+        break
 
-for i in range(second, n-1):
-    if index >= n-1:
+# print(f'index = {index} first = {first} second = {second} third = {third}')
+for i in range(index, n-1):
+    if sum(arr[i]) == sum(arr[i+1]):
+        index += 1
+        third += 2
+    if index > n-1:
         break
     else:
         index += 1
         third += 1
-    if sum(arr[i]) == sum(arr[i+1]):
-       index += 1
-       third += 1
-print(index, first, second, third)
+        break
+# print(f'index = {index} first = {first} second = {second} third = {third}')
 
-print(first*1000000)
-print(second*500000)
-print(third*100000)
+first *=100
+second *=80
+third *=50
+print(first+second+third)
