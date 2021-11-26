f8349fa37b84b8ff641fa8143974c4d9a300d5f9
junho5 <dhwnsghdhkd@naver.com>
2021-11-26 22:56:20 +0900

    test peroid

--- implement/implement_02.py
+++ implement/implement_02.py
@@ -6,8 +6,6 @@ cnt = 0
 for i in range(0, N+1):
     for j in range(10):
         for k in range(10):
-            if '3' in str(i) + str(j) + str(k): 
+            if '3' in str(i) + str(j) + str(k):
                 cnt += 1
 print(cnt)
-
-
