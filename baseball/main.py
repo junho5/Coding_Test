19990f8c7d9cfddd3239797b20075b007ea9712c
junho5 <dhwnsghdhkd@naver.com>
2022-01-19 23:30:48 +0900

    baseball finish

--- baseball/main.py
+++ baseball/main.py
@@ -1,6 +1,5 @@
 from random import randint
 
-
 def generate_numbers():
     numbers = []
 
@@ -11,7 +10,6 @@ def generate_numbers():
 
     print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
     return numbers
-print(generate_numbers())
 
 def take_guess():
     print("숫자 3개를 하나씩 차례대로 입력하세요.")
@@ -26,8 +24,37 @@ def take_guess():
         else:
             new_guess.append(num)
             
-            
-        
     return new_guess
 
-print(take_guess())
+
+def get_score(guesses, solution):
+    strike_count = 0
+    ball_count = 0
+    for i in range(3):
+        for j in range(3):
+            if guesses[i] == solution[j]:
+                if guesses[i] == solution[i]:
+                    strike_count +=1
+                else:
+                    ball_count +=1
+            else:
+                continue
+    return strike_count,ball_count
+    
+ANSWER = generate_numbers()
+tries = 0
+
+while True:
+    people = take_guess()
+    s,b = get_score(people,ANSWER)
+    print(f'{s} {b}')
+    tries +=1
+    
+    if s ==3:
+        break
+    
+    
+print("축하합니다. {}번 만에 숫자 3개의 값과 위치를 모두 맞추셨습니다.".format(tries))
+
+    
+
