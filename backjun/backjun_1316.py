f1a34458a26841994d91016e3b3b214d14cd1e07
junho5 <dhwnsghdhkd@naver.com>
2022-02-20 22:49:27 +0900

    update

--- backjun/backjun_1316.py
+++ backjun/backjun_1316.py
@@ -1,8 +1,8 @@
-# time = int(input())
-# answer = 0
-# for i in range(time):
-#     word = list(input())
+time = int(input())
+answer = 0
+for i in range(time):
+    word = list(input())
             
-# print(answer)
+print(answer)
         
     
