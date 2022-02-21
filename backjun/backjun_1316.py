2c24eef2d4888f7c659eae9423d75ed8bf92c1a6
junho5 <dhwnsghdhkd@naver.com>
2022-02-22 01:15:50 +0900

    1316 finish

--- backjun/backjun_1316.py
+++ backjun/backjun_1316.py
@@ -1,11 +1,22 @@
 time = int(input())
+error = 0
 answer = []
+score = 0
+
 for i in range(time):
     word = list(input())
-    unique_word = set(word)
-    for i in range(len(unique_word)):
-        
+    for j in set(word):
+        for k in range(len(word)):
+            if j == word[k] and j not in answer:
+                answer.append(k)
+        for l in range(len(answer)-1):
+            if abs(answer[l] - answer[l+1]) > 1:
+                error += 1
+        answer = []
+    if error == 0 :
+        score += 1
+    else:
+        error = 0
+
+print(score)
             
-print(answer)
-        
-    
