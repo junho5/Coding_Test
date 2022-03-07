75aa558c1eb359848c192567962c0f9ce8805ea8
junho5 <dhwnsghdhkd@naver.com>
2022-03-07 16:06:28 +0900

    1002 finish study

--- backjun/backjun_1002.py
+++ backjun/backjun_1002.py
@@ -1,5 +1,25 @@
+import math
+
 n = int(input())
 
 for i in range(n):
     x1,y1,r1,x2,y2,r2 = map(int,input().split())
     
+    distance = math.sqrt((x2-x1)**2+(y2-y1)**2)
+
+    if distance == 0:
+        if r1 == r2:
+            print(-1)
+        else:
+            print(0)
+    else:
+        if distance == r1 + r2 or distance == abs(r1-r2):
+            print(1)
+        elif distance < r1+r2 and distance > abs(r1-r2):
+            print(2)
+        else:
+            print(0)
+
+
+
+
--- test/test.py
+++ test/test.py
@@ -1,11 +1,5 @@
-while(True):
-    answer = list(map(int,input().split()))
-    answer.sort()
-    if answer[0] ==0 and answer[1] ==0 and answer[2] ==0:
-        break
-    elif answer[2]**2 == answer[0]**2 + answer[1]**2:
-        print('right')
-    else:
-        print('wrong')
+a=[[2,1],[3,2]]
+b=[[3,2],[1,1]]
 
-
+if a[1] in b:
+    print('확인')
