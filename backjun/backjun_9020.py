eeaaa0f6af2979e61e78bb4f8527b827bcf14019
junho5 <dhwnsghdhkd@naver.com>
2022-03-09 01:54:19 +0900

    9020 finish

--- backjun/backjun_9020.py
+++ backjun/backjun_9020.py
@@ -1,34 +1,19 @@
 import math
 
+result = [True] * 10001
+for i in range(2,int(math.sqrt(10001))):
+    if result[i] == True:
+        for j in range(i+i,10001,i):
+            result[j] = False
+
+prime = [i for i in range(2,10001) if result[i]==True]
+            
 n = int(input())
-answer = []
-store = 100
+
 for i in range(n):
     num = int(input())
-    result = [True] * (num + 1)
-    
-    for j in range(2,int(math.sqrt(num)+1)):
-        if result[j] == True:
-            for k in range(j+j,num+1,j):
-                result[k] = False
-                
-    prime = [i for i in range(2,num+1) if result[i]==True]
-    
-    # print(prime)
-    
-    for i in range(len(prime)):
-        for j in range(len(prime)):
-            temp = prime[i] + prime[j]
-            if temp == num:
-                minus = abs(prime[i]-prime[j])
-                if store > minus:
-                    store = minus
-                    answer = []
-                    answer.append(prime[i])
-                    answer.append(prime[j])             
-    for i in answer:
-        print(i, end=' ')
-    print('')
-    answer = []
-    store = 100
+    for j in range(num//2,num+1):
+        if num-j in prime and j in prime:
+            print(num-j,j)
+            break 
     
--- test/test.py
+++ test/test.py
@@ -1,2 +1,29 @@
-a= [[23,3],[2.3]]
+import math
+import sys
 
+result = [True] * 10001
+for i in range(2,int(math.sqrt(10001))):
+    if result[i] == True:
+        for j in range(i+i,10001,i):
+            result[j] = False
+            
+n = a = int(sys.stdin.readline())
+
+for i in range(n):
+    answer = []
+    minus = []
+    num = int(sys.stdin.readline())
+
+    prime = [i for i in range(2,num+1) if result[i]==True]
+    # print(prime)
+    index = len(prime) // 2
+    for j in range(index,len(prime)):
+        if num - prime[j] in prime:
+            answer.append([prime[j],num-prime[j]])
+            minus.append(prime[j]-(num-prime[j]))
+    min_index = minus.index(min(minus))
+    answer[min_index].sort()
+    for i in answer[min_index]:
+        print(i, end = ' ')
+    print('')
+        
