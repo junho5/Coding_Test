92822846cae0b39f44cb79c78feaece23ba6d77f
junho5 <dhwnsghdhkd@naver.com>
2022-03-09 23:37:23 +0900

    recursive

--- test/test.py
+++ test/test.py
@@ -1,9 +1,20 @@
-def fact(n):
-    if n <= 1:
+def fivo(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n== 2:
         return 1
     else:
-        return n * fact(n-1)
-
+        return fivo(n-1) + fivo(n-2)
+        
+def pact (m):
+    if m <1:
+        return 1
+    else:
+        return m * pact(m-1)
 
 n = int(input())
-print(fact(n))
+print(fivo(n))
+
+        
+m = int(input())
+print(pact(m))
