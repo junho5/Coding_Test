72e08d3484560c54ff55b1642062dda09833b4a5
junho5 <dhwnsghdhkd@naver.com>
2022-03-10 23:18:16 +0900

    update

--- test/test.py
+++ test/test.py
@@ -1,20 +1,20 @@
-def fivo(n):
-    if n == 0:
-        return 0
-    elif n == 1 or n== 2:
-        return 1
-    else:
-        return fivo(n-1) + fivo(n-2)
+# def fivo(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1 or n== 2:
+#         return 1
+#     else:
+#         return fivo(n-1) + fivo(n-2)
         
-def pact (m):
-    if m <1:
-        return 1
-    else:
-        return m * pact(m-1)
+# def pact (m):
+#     if m <1:
+#         return 1
+#     else:
+#         return m * pact(m-1)
 
-n = int(input())
-print(fivo(n))
+# n = int(input())
+# print(fivo(n))
 
         
-m = int(input())
-print(pact(m))
+# m = int(input())
+# print(pact(m))
